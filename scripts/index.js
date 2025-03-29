5b4c5c5dd08db15a6f59da5b7f208091b5ef3866
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-03-29 15:23:42 +0000

    initial design

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 import { getCookieValue } from "./modules/cookies.js";
+import { includeHTML } from "./modules/utils.js";
 
 // check if warning read
 if ((getCookieValue("has_read_warning") !== "true") && (window.location.pathname != "/warning.html")) {
     window.location.href="/warning.html?redir=" + encodeURIComponent(window.location.pathname)
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    includeHTML();
+});
